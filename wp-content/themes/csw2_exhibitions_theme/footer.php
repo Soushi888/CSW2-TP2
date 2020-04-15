241782ed978b3a3ef4ac623dfc76ce1d388ab392
Soushi888 <Sacha.pignot@gmail.com>
2020-04-15 14:56:57 -0400

    Update footer.php

--- wp-content/themes/csw2_exhibitions_theme/footer.php
+++ wp-content/themes/csw2_exhibitions_theme/footer.php
@@ -15,17 +15,7 @@
 
 	<footer id="colophon" class="site-footer">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'csw2_exhibitions_theme' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'csw2_exhibitions_theme' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'csw2_exhibitions_theme' ), 'csw2_exhibitions_theme', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
+			<p>&copy; 2020 <?= get_bloginfo('name', 'csw2_exhibitions_theme') ?> &mdash; Tout droits réservés.</p>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
